8572330fad90149f37a7257a28cdcf79270e39a4
Barış <barisalbayrak34@gmail.com>
2021-03-09 20:31:25 +0300

    baris

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,29 @@ function matchMediaQuery(mQuAu) {
         }
     } */
 
+    
+    if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 1000;
+	var distance = 1000;
+    
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time );
+}
+
 
     let halfOp = document.querySelector('video');
     let animaT0 = document.getElementById('animaT0');
@@ -56,7 +79,7 @@ function matchMediaQuery(mQuAu) {
     $(document).ready(function () {
         $('.arg0_swiper').slick({
             autoplay: true,
-            autoplaySpeed: 1000,
+            autoplaySpeed: 3000,
             arrows: false,
             lazyLoad: 'ondemand',
             adaptiveHeight: true,
@@ -68,3 +91,117 @@ function matchMediaQuery(mQuAu) {
             $('.arg0_swiper').slick('slickNext');
         });
     });
+
+    $(document).ready(function () {
+        $('.arg0_swiper-two').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            lazyLoad: 'ondemand',
+            adaptiveHeight: true,
+        });
+        $('#arg0_swiper-two--prev').on('click', function () {
+            $('.arg0_swiper-two').slick('slickPrev');
+        });
+        $('#arg0_swiper-two--next').on('click', function () {
+            $('.arg0_swiper-two').slick('slickNext');
+        });
+    });
+
+    $(document).ready(function () {
+        $('.arg0_swiper-three').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows: false,
+            lazyLoad: 'ondemand',
+            adaptiveHeight: true,
+        });
+    });
+
+
+    $(document).ready(function () {
+        $('.arg0_swiper-four').slick({
+            autoplay: false,
+            autoplaySpeed: 2000,
+            arrows: false,
+            lazyLoad: 'ondemand',
+            adaptiveHeight: true,
+        });
+        $('#arg0_swiper-four--prev').on('click', function () {
+            $('.arg0_swiper-four').slick('slickPrev');
+        });
+        $('#arg0_swiper-four--next').on('click', function () {
+            $('.arg0_swiper-four').slick('slickNext');
+        });
+    });
+
+
+// smoothScroll-Es5.js | Start |
+
+"use strict";
+window.addEventListener("load", function () {
+    function scrollIt(destination, duration, easing) {
+        if (destination == null) {
+            console.log("scroll destination: " + destination);
+            return;
+        }
+        var easings = {
+            "linear": function (t) {
+                return t;
+            },
+            "smooth": function (t) {
+                return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+            }
+        };
+
+        var start = window.pageYOffset;
+        var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+        var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+        var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+        var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+        if ('requestAnimationFrame' in window === false) {
+            window.scroll(0, destinationOffsetToScroll);
+            return;
+        }
+
+        function scroll() {
+            var now = 'now' in window.performance ? performance.now() : new Date().getTime();
+            var time = Math.min(1, ((now - startTime) / duration));
+            var timeFunction = easings[easing](time);
+            window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+            if (Math.abs(window.pageYOffset - destinationOffsetToScroll) < 1) {
+                return;
+            }
+            requestAnimationFrame(scroll);
+        }
+        scroll();
+    }
+
+    var all_scroll_on_click_elements = document.getElementsByClassName("scrollOnClick");
+    if (all_scroll_on_click_elements.length != 0) {
+        for (var i = 0; i < all_scroll_on_click_elements.length; i++)
+            all_scroll_on_click_elements[i].addEventListener('click',
+                function (e) {
+                    var d = e.currentTarget;
+                    var duration = d.getAttribute("duration");
+                    if (duration == "" || duration == null) duration = 500;
+                    duration = parseInt(duration);
+                    var easing = d.getAttribute("easing");
+                    if (easing == "" || easing == null) easing = "smooth";
+                    var id = d.getAttribute("scrollTo");
+                    scrollIt(
+                        document.getElementById(id),
+                        duration,
+                        easing
+                    );
+                });
+    } else {
+        console.log("WARNING: No elements with class scrollOnClick found.");
+    }
+});
+
+// smoothScroll-Es5.js | End |
